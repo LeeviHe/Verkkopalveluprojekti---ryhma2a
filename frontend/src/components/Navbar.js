968d910c219b17dd149985b72fac8deab4d1dd94
Riku Björn <riku.bjorn@gmail.com>
2022-11-07 21:59:23 +0200

    värikoodit ja yritys nav baarin kuvasta

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
             <li className="nav-item">
               <form>
                 <input type="search" classNameName="form-control-dark text-bg-dark me-2" placeholder="Search..." aria-label="Search"></input>
-                <button className="nav-item">Search</button>
+                <button className="nav-item"><img src = "https://cdn-icons-png.flaticon.com/512/149/149852.png"></img></button>
               </form>
             </li>
 
@@ -31,13 +31,12 @@ export default function Navbar() {
   );
 }
 
-/*
-search
+
+/*search
 https://www.flaticon.com/free-icon/search_149852?term=search&page=1&position=2&page=1&position=2&related_id=149852&origin=search
 
 account
 https://www.flaticon.com/free-icon/user_1144760?related_id=1144760&origin=search
 
 cart
-https://www.flaticon.com/free-icon/shopping-cart_3144456?term=cart&page=1&position=2&page=1&position=2&related_id=3144456&origin=style
-*/
+https://www.flaticon.com/free-icon/shopping-cart_3144456?term=cart&page=1&position=2&page=1&position=2&related_id=3144456&origin=style*/
