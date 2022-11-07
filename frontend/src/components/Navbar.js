cbdde66ea869261b328646dadb4f1028e3c39957
MiaaaV <sirivesa@gmail.com>
2022-11-07 22:01:42 +0200

    main carousel functionality ok

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <>
 
-      <div className="container">
+      <div className="container navbar-container">
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <img src={img} className="main-logo" alt="Shoelando logo" />
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+import red from '../images/testred.jpg';
+import blue from '../images/testblue.jpg';
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
-      <p>asdadadsa</p>
+      <div className='carousel-container'>
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src={red} alt="picture"></img>
+
+              <div class="container">
+                <div class="carousel-caption text-start">
+                  <h1>Example headline.</h1>
+                  <p>Some representative placeholder content for the first slide of the carousel.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={blue} alt="picture"></img>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Another example headline.</h1>
+                  <p>Some representative placeholder content for the second slide of the carousel.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </>
   )
 }
