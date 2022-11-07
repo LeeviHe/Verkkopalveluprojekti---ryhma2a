18b6207ada87629b9549c87d36a3f929b80feace
MiaaaV <sirivesa@gmail.com>
2022-11-07 20:35:29 +0200

    search button added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,17 +10,20 @@ export default function Navbar() {
 
       <div className="container">
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <img src={img} className="main-logo" alt="Shoelando logo" />
           </a>
 
-          <ul class="nav nav-pills">
-            <li class="nav-item">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
               <form>
-                <input type="search" className="form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"></input>
-              </form></li>
-            <li class="nav-item">Account</li>
-            <li class="nav-item">Cart</li>
+                <input type="search" classNameName="form-control-dark text-bg-dark me-2" placeholder="Search..." aria-label="Search"></input>
+                <button className="nav-item">Search</button>
+              </form>
+            </li>
+
+            <li className="nav-item me-2">Account</li>
+            <li className="nav-item me-2">Cart</li>
           </ul>
         </header>
       </div>
@@ -29,6 +32,12 @@ export default function Navbar() {
 }
 
 /*
+search
+https://www.flaticon.com/free-icon/search_149852?term=search&page=1&position=2&page=1&position=2&related_id=149852&origin=search
+
+account
 https://www.flaticon.com/free-icon/user_1144760?related_id=1144760&origin=search
+
+cart
 https://www.flaticon.com/free-icon/shopping-cart_3144456?term=cart&page=1&position=2&page=1&position=2&related_id=3144456&origin=style
 */
