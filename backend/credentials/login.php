acf87878452024b9de723a4df3cd42fa25b8a06c
MiaaaV <sirivesa@gmail.com>
2022-12-18 17:16:25 +0200

    sanitized login and register php inputs

--- backend/credentials/login.php
+++ backend/credentials/login.php
@@ -15,6 +15,39 @@ if (!isset($_POST['logemail']) || !isset($_POST["logpassword"])) {
     return;
 }
 
+// sanitizing user input
+
+$username = filter_input(INPUT_POST, 'logemail', FILTER_SANITIZE_EMAIL);
+$password = filter_input(INPUT_POST, 'logpassword', FILTER_SANITIZE_STRING);
+$verified_user = checkLogin($username, $password);
+
+try {
+    $db = openDb();
+    $stmt = $db->prepare("SELECT * FROM customer WHERE email = :email");
+
+    $stmt->bindParam(':email', $username);
+    $stmt->execute();
+
+    $user = $stmt->fetch();
+
+    if ($verified_user) {
+      $_SESSION["username"] = $verified_user;
+      http_response_code(200);
+      echo htmlspecialchars($verified_user), ENT_NOQUOTES;
+
+    } else {
+      http_response_code(401);
+      echo "Väärä sähköpostiosoite tai salasana.";
+    }
+
+  } catch (PDOException $pdoex) {
+    returnError($pdoex);
+  }
+
+/*
+
+old user login input
+
 $username = $_POST['logemail'];
 $password = $_POST['logpassword'];
 
@@ -26,4 +59,4 @@ if ($verified_user) {
 } else {
     http_response_code(401);
     echo "Väärä sähköpostiosoite tai salasana.";
-}
+}*/
